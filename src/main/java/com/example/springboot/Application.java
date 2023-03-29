2eefff74533dbbd2ce55997801eafffea1c1e4af
Anton Kozlov <akozlov@azul.com>
2023-03-29 14:12:32 +0300

    Import clean gs-sprin-boot/initial @ c42d4edfec8e7
    https://github.com/spring-guides/gs-spring-boot/tree/c42d4edfec8e704431380b884f5cfed78f17e876/initial

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -11,6 +11,14 @@ public class Application {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+		System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
 	}
 	
 }
